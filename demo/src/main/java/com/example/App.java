b4ae18d66af26f7cc7ee9dea37ed02ffe3a82f7f
Kike Alcaraz-Briseño <ralcarazbriseno@gmail.com>
2025-02-17 18:34:59 -0800

    included switch statement

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -9,10 +9,12 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
+    
         //Create calculator format
         System.out.println("---------------------------------------------------");
         System.out.println("Chavvi Calc");
         System.out.println("---------------------------------------------------");
+        // declare A and B as a variable
         System.out.println("A = 0.000   B = 0.000           ");
         System.out.println("---------------------------------------------------");
         System.out.println("a       Enter a value for A");
@@ -25,8 +27,38 @@ public class App
         System.out.println("q       Quit");
         System.out.println("---------------------------------------------------");
 
-        //Create Scanner object
-        Scanner scanner = new Scanner(System.in);
+        //create Scanner object
         System.out.print("Enter a command: ");
+        Scanner scanner = new Scanner(System.in);
+        char operator = scanner.next().charAt(0); 
+
+        //ask user for numberOne
+        System.out.print("Enter a number: ");
+        double number1 = scanner.nextDouble();
+
+        System.out.print("Enter a number: ");
+        double number2 = scanner.nextDouble();
+
+        double result = 0.0;
+        switch(operator) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                if (number2 != 0) {
+                    result = number1 / number2; 
+                } else {
+                    System.out.println("Error: You cannot divide by 0");
+                    return;
+                }
+                break;
+        }
+        System.out.println("Result: " + result);
     }
 }
