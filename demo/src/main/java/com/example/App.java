7425df67545495c50313ae353274d545d111cea2
Kike Alcaraz-Briseño <ralcarazbriseno@gmail.com>
2025-02-20 20:33:52 -0800

    completed program

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -6,8 +6,7 @@ package com.example;
  */
 import java.util.Scanner;
 
-public class App 
-{
+public class App {
     public static void main( String[] args ) {
         //create scanner class
         Scanner scanner = new Scanner(System.in);
@@ -15,9 +14,12 @@ public class App
         //initialize A and B to 0
         double valueA = 0.0;
         double valueB = 0.0;
+        double result = 0.0;
+
+        boolean continueLoop = true;
 
         //start while(true) loop
-        while (true){
+        while (continueLoop){
             //display the current value of the calculator
             String formattedA = String.format("%.3f", valueA);
             String formattedB = String.format("%.3f", valueB);
@@ -25,7 +27,6 @@ public class App
             System.out.println("---------------------------------------------------");
             System.out.println("Chavvi Calc");
             System.out.println("---------------------------------------------------");
-            // declare A and B as a variable
             System.out.println("A = " + formattedA + "         B = " + formattedB);
             System.out.println("---------------------------------------------------");
             System.out.println("a       Enter a value for A");
@@ -39,50 +40,66 @@ public class App
             System.out.println("---------------------------------------------------");
             System.out.print("Enter a command: ");
 
-            String input = scanner.nextLine();
-            String trimmedInput = input.trim();
-            String command = trimmedInput.toLowerCase();
+            // String input = scanner.nextLine();
+            //String trimmedInput = input.trim();
+            //String command = trimmedInput.toLowerCase();
 
+            char operator = scanner.next().charAt(0); 
+            
+            switch(operator) {
+                case 'a':
+                    System.out.print("Enter a number: ");
+                    try {
+                        valueA = scanner.nextDouble();
+                    } catch (Exception e) {
+                        System.out.println("Error: The value entered is not a double");
+                        scanner.nextLine();
+                    }
+                    result = valueA;
+                    break;
+                case 'b':
+                    System.out.print("Enter a number: ");
+                    try {
+                        valueB = scanner.nextDouble();
+                    } catch (Exception e) {
+                        System.out.println("Error: The value entered is not a double");
+                        scanner.nextLine();
+                    }
+                    break;
+                case '+':
+                    result = valueA + valueB;
+                    break;
+                case '-':
+                    result = valueA - valueB;
+                    break;
+                case '*':
+                    result = valueA * valueB;
+                    break;
+                case '/':
+                    if (valueB != 0) {
+                        result = valueA / valueB; 
+                    } else {
+                        System.out.println("Error: Unable to divide by 0");
+                    }
+                    break;
+                case 'c':
+                    result = 0.000;
+                    valueB = 0.000;
+                    break;
+                case 'q':
+                    continueLoop = false;
+                    System.out.print("Thank you for using Chavvi's Calculator!");
+                    break;  
+                default:
+                    System.out.println("Error");
+            }
+            valueA = result;
+        }
+    }
+}
+ /*
             switch (command) {
                 case "a":
                     System.out.print("Enter value for A: ");
             }
-        }
-            
-        // //create Scanner object
-        // System.out.print("Enter a command: ");
-      
-        // char operator = scanner.next().charAt(0); 
-
-        // //ask user for numberOne
-        // System.out.print("Enter a number: ");
-        // double number1 = scanner.nextDouble();
-
-        // System.out.print("Enter a number: ");
-        // double number2 = scanner.nextDouble();
-
-        // double result = 0.0;
-        // switch(operator) {
-        //     case '+':
-        //         result = number1 + number2;
-        //         break;
-        //     case '-':
-        //         result = number1 - number2;
-        //         break;
-        //     case '*':
-        //         result = number1 * number2;
-        //         break;
-        //     case '/':
-        //         if (number2 != 0) {
-        //             result = number1 / number2; 
-        //         } else {
-        //             System.out.println("Error: You cannot divide by 0");
-        //             return;
-        //         }
-        //         break;
-        //     default:
-        //         System.out.println("Error");
-        // }
-        // System.out.println("Result: " + result);
-    }
-}
+                    */
